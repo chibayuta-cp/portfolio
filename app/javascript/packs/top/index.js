29847857dc852bb7e4e494e3d532abeddcda4d17
chiba yuta <yyyyyuta.c.i.s.0401@gmail.com>
2021-04-12 00:24:21 +0900

    stay

--- app/javascript/packs/top/index.js
+++ app/javascript/packs/top/index.js
@@ -4,31 +4,38 @@
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
 
-
-
 $(function() {
- $('#form').submit(function (event) {
-              var formData = $('#form').serialize();
-              $.ajax({
-                url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSefiWrmnKtuVPNLERD_axHCs3DnlOqavVYZVEaHm7YtGkg-XA/formResponse",
-                data: formData,
-                type: "POST",
-                dataType: "xml",
-                statusCode: {
-                  0: function () {
-                    $(".end-message").slideDown();
-                    $(".submit-btn").fadeOut();
-                    //window.location.href = "thanks.html";
-                  },
-                  200: function () {
-                    $(".false-message").slideDown();
+  //お問い合わせフォームページ移動しないように設定
+    $('#form').submit(function (event) {
+                var formData = $('#form').serialize();
+                $.ajax({
+                  url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSefiWrmnKtuVPNLERD_axHCs3DnlOqavVYZVEaHm7YtGkg-XA/formResponse",
+                  data: formData,
+                  type: "POST",
+                  dataType: "xml",
+                  statusCode: {
+                    0: function () {
+                      $(".end-message").slideDown();
+                      $(".submit-btn").fadeOut();
+                      //window.location.href = "thanks.html";
+                    },
+                    200: function () {
+                      $(".false-message").slideDown();
+                    }
                   }
-                }
+                });
+                event.preventDefault();
               });
-              event.preventDefault();
-            });
   
-            //AOS.jsの設定
-       AOS.init()
-
+      //AOS.jsの設定
+         AOS.init()
+      //ページ内リンク調整
+         var headerHeight = 60;//固定ヘッダーの高さを入れる
+      $('[href^="#"]').click(function(){
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top-headerHeight;
+        $("html, body").animate({scrollTop:position}, 1000, "swing");//200はスクロールの移動スピードです
+        return false;
+      });
 }
